eca6fe517bebf15a55d6e91b049227e30ecbdca1
boredream <48262906@qq.com>
2016-08-13 16:47:43 +0800

    junit测试部分
    mockito

--- app/src/test/java/com/boredream/designrescollection/ExampleUnitTest.java
+++ app/src/test/java/com/boredream/designrescollection/ExampleUnitTest.java
@@ -2,14 +2,33 @@ package com.boredream.designrescollection;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void mockitoTest() throws Exception {
+        // mock creation
+        List mockedList = mock(List.class);
+
+        // using mock object - it does not throw any "unexpected interaction" exception
+        mockedList.add("one");
+        mockedList.clear();
+
+        // selective, explicit, highly readable verification
+        verify(mockedList).add("one");
+        verify(mockedList).clear();
     }
 }
